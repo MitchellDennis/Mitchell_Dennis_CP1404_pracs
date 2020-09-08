cee3152e2d4418c572c14c75474a8b0d8680151e
MitchellDennis <68805371+MitchellDennis@users.noreply.github.com>
2020-09-08 11:20:49 +1000

    finished __str__

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -15,4 +15,7 @@ class ProgrammingLanguage:
             return True
         else:
             return False
+    def __str__(self):
+        return "{}, {} Typing, Reflection = {}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year)
+
 
