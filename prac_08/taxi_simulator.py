180abfa6784584e7a1ad316b9f234c1ff0f09050
MitchellDennis <68805371+MitchellDennis@users.noreply.github.com>
2020-10-04 17:11:47 +1000

    Finished do from scratch excersise 08

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,44 @@
+
+from prac_08.taxi import Taxi
+from prac_08.silverservicetaxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Toytoa", 200, 4)]
+
+    print("Let's go!")
+    print(MENU)
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            print("Taxis available: ")
+            display_taxis(taxis)
+            current_taxi = taxis[int(input("Choose taxi: "))]
+        elif menu_choice == "d":
+            current_taxi.start_fare()
+            distance_to_drive = float(input("Drive how far? "))
+            current_taxi.drive(distance_to_drive)
+            trip_cost = current_taxi.get_fare()
+            print("Your {} trip cost you ${:.2f}".format(current_taxi.name,
+                                                         trip_cost))
+            total_bill += trip_cost
+        else:
+            print("Invalid option")
+        print("Bill to date: ${:.2f}".format(total_bill))
+        print(MENU)
+        menu_choice = input(">>> ").lower()
+
+    print("Total trip cost: ${:.2f}".format(total_bill))
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, taxi))
+
+main()
